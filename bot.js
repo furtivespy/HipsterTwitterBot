b9842ed5116133bf0a41482e522490320692ce32
Will <furtivespy@gmail.com>
2016-05-20 21:21:51 -0400

    More

--- bot.js
+++ bot.js
@@ -2,14 +2,17 @@ var deck = require('deck');
 var Twit = require('twit');
 
 var T = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...'
+  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+  access_token:         process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
+
 
 function newBusiness(){
 	T.post('statuses/update', { status: 'Hello World!' }, function(err, data, response) {
 		console.log(data);
 	});
 }
+
+newBusiness();
