f6d64c4ec2bbd5f60ad35b9edf5e609dd60eb275
Will <furtivespy@gmail.com>
2016-06-27 19:35:07 -0400

    more words

--- bot.js
+++ bot.js
@@ -8,23 +8,23 @@ var T = new Twit({
   access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
-var NameWords = ["amber", "anchor", "awl", "apple", "argyle", "armor", "arrow", "badger", "ballast", "branch",
-				 "balsam", "bantam", "barber", "barley", "barrow", "basil", "basin", "basket", "battle", "bowl",
+var NameWords = ["amber", "anchor", "ankh", "awl", "apple", "argyle", "armor", "arrow", "badger", "ballast", "branch",
+				 "balsam", "bantam", "barber", "barley", "barrow", "basil", "basin", "basket", "battle", "bowl", "beast",
 				 "beauty", "bedroom", "beetle", "beggar", "berry", "birdcage", "bishop", "blossom", "bluebird", 
 				 "blueprint", "bonnet", "bourbon", "breakfast", "brother", "bubble", "bucket", "buoy", "horn",
-				 "burlap", "butler", "butter", "button", "cabbage", "cable", "cactus", "camphor", "candle", 
+				 "burlap", "butler", "butter", "button", "cabbage", "cable", "cactus", "camphor", "candle", "coyote",
 				 "cannon", "canvas", "captain", "caption", "carrot", "castle", "cattle", "cedar", "cellar", 
 				 "channel", "cheddar", "cherry", "chicken", "hair", "cider", "cipher", "circle", "clover", 
 				 "cobalt", "cobweb", "cocoa", "coil", "colonel", "column", "compass", "copper", "cotton", "corn",
 				 "country", "cracker", "creature", "cycle", "daisy", "danger", "daughter", "denim", "diamond", 
-				 "dinner", "doctor", "dollar", "donkey", "dovetail", "dutchess", "eagle", "earthquake", "fossil",
-				 "empress", "engine", "family", "father", "faucet", "favor", "feather", "fiddle", "fiddler", "funk",
+				 "dinner", "doctor", "dollar", "donkey", "dovetail", "dutchess", "eagle", "earthquake", "fossil", "hinge",
+				 "empress", "engine", "family", "father", "faucet", "favor", "feather", "fiddle", "fiddler", "funk", "gutter",
 				 "flower", "fortune", "freckle", "galley", "gannet", "garnet", "gingham", "goblin", "goldfish", "foam",
-				 "hammer", "hazel", "heather", "hobby", "holly", "honey", "jackal", "jacket", "jewel", "justice", 
-				 "kerning", "lady", "lawyer", "leather", "leopard", "letter", "lettuce", "linen", "lion", "lace",
-				 "lobster", "locket", "lumber", "magic", "marble", "market", "marlin", "marrow", "mayor", "meta",
+				 "hammer", "hazel", "heather", "hobby", "holly", "honey", "jackal", "jacket", "jewel", "justice", "king",
+				 "kerning", "lady", "lawyer", "leather", "leopard", "letter", "lettuce", "linen", "lion", "lace", "lustre",
+				 "lobster", "locket", "lumber", "magic", "marble", "market", "marlin", "marrow", "mayor", "meta", "octopus",
 				 "pork", "mitten", "money", "morning", "mortar", "mother", "mountain", "mushroom", "music", "night",
-				 "nation", "needle", "nickel", "notebook", "novel", "number", "nutmeg", "oatmeal", "ocean", 
+				 "nation", "needle", "nickel", "notebook", "novel", "number", "nutmeg", "oatmeal", "ocean", "peach",
 				 "opal", "owl", "oxen", "paintbrush", "painter", "paper", "parcel", "parent", "parsley", "goose",
 				 "pebble", "pencil", "penny", "pepper", "pickle", "picture", "pistol", "pocket", "poem", "soak",
 				 "poison", "porter", "potter", "pulley", "quicksand", "quiet", "quiver", "rabbit", "railway", "skip",
@@ -49,7 +49,10 @@ var Descriptors = ["artisanal", "artisan", "asymmetrical", "bearded", "trendy", 
 				   "silky", "charming", "nebulous", "buttery", "onerous", "zany", "damaged", "imperfect", "abandoned", "furtive", "dusty",
 				   "obsolete", "accessible", "goofy", "savory", "ethereal", "rhetorical", "knotty", "fragile", "dramatic", "rural", "selective",
 				   "hallowed", "haunted", "jalapeno", "simple", "kindhearted", "rebellious", "lucky", "alcoholic", "heartbreaking", "disillusioned",
-				   "sophisticated", "fermented"  ];
+				   "sophisticated", "fermented", "liberated", "charismatic", "brick", "fresh", "contemporary", "androgynous", "conceptual", 
+				   "ambient", "delicate", "guerilla", "unrecognizable", "decorative", "musical", "wild", "miniature", "extinct", "series of", 
+				   "relatable", "social", "charitable", "ethnic", "wind-powered", "metallic", "fluoridated", "semicontinuous", "semiclassical",
+				   "precivilization", "ante mortem", "poisonous", "vectible"    ];
 
 var Products = ["cardigans", "glasses", "tattoos", "cocktails", "fedoras", "bicycles", "kale", "leggings", "mustaches", "whiskey", "beer", 
 				"trucker hats", "synthesizers", "bacon", "scooters", "high-top sneakers", "baristas", "urban farming", "grilled cheese sandwiches",
@@ -61,7 +64,9 @@ var Products = ["cardigans", "glasses", "tattoos", "cocktails", "fedoras", "bicy
 				"toothbrushes", "dental floss", "razors", "shaving foam", "rice pudding", "toys", "toothpaste", "envelopes", "eggnog", "umbrellas",
 				"ice cubes", "jellies and jams", "pizza", "beef", "mittens", "juice", "smoothies", "breads", "oatmeal", "pencils", "yo-yos",
 				"whistles", "soaps", "crackers", "eggs", "popcorn", "cheese", "guitars", "quinoa", "yogurt", "sriracha", "cupcakes", "pistachios",
-				"avocados", "chocolate", "nutella", "vodka", "honey", "headbands", "jorts", "overalls", "soups", "stews"   ];
+				"avocados", "chocolate", "nutella", "vodka", "honey", "headbands", "jorts", "overalls", "soups", "stews", "sleep masks", "airships",
+				"handlebars", "glacier water", "steaks", "grandfather clocks", "news", "stamps", "polaroid cameras", "chili", "housedresses",
+				"cookies", "grits"	   ];
 
 String.prototype.capitalizeFirstLetter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
